d6e844efbc71e8503443176869f3d4615382f6d5
marcoslora <marcoslora95@gmail.com>
2022-07-07 18:07:55 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
+from passw import FB_PASSWORD
+
+FB_EMAIL = "xdevelopm@gmail.com"
+
+
+chrome_driver_path = "/Users/ml/Documents/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("http://www.tinder.com")
+
+sleep(2)
+login_button = driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/span')
+login_button.click()
+
+sleep(2)
+fb_login = driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div[1]/div/div/div[3]/span/div[2]/button/span[2]')
+fb_login.click()
+
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+email = driver.find_element(By.XPATH, '//*[@id="email"]')
+password = driver.find_element(By.XPATH, '//*[@id="pass"]')
+
+email.send_keys(FB_EMAIL)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+driver.switch_to.window(base_window)
+print(driver.title)
+
+sleep(5)
+allow_location_button = driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div/div/div[3]/button[1]/span')
+allow_location_button.click()
+notifications_button = driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div/div/div[3]/button[1]/span')
+notifications_button.click()
+cookies = driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[2]/div/div/div[1]/div[1]/button/span')
+cookies.click()
+
+for n in range(100):
+    sleep(1)
+    try:
+        print("called")
+        like_button = driver.find_element(By.XPATH,
+            '//*[@id="q-996647900"]/div/div[1]/div/div/main/div/div/div[1]/div/div[4]/div/div[4]/button/span/span/svg/path')
+        like_button.click()
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
+            match_popup.click()
+        except NoSuchElementException:
+            sleep(2)
+
+driver.quit()
